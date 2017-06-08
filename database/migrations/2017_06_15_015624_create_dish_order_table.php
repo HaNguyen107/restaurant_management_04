57589c8e67ee8129f370c9d4cdd6faacd4b5422b
HaNguyen107 <nguyenthiha107.cmu@gmail.com>
2017-06-08 16:08:30 +0700

    create migration

---
+++ database/migrations/2017_06_15_015624_create_dish_order_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDishOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dish_order', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('quantity');
+            $table->float('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dish_order');
+    }
+}
